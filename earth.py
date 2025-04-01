e2ee52df8327e2c0df26b0171db83b66fe4ce16f
Joacobrule <jbrunengo@mail.austral.edu.ar>
2025-04-01 16:56:53 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,10 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    Merme = x>y
+    Bruno = y>x
+    Merme = str(Merme)
+    Bruno = str(Bruno)
+    print("The result of "+x+" comes first in the dictionary than "+y+" is "+Bruno+".")
+    print("The result of "+y+" comes first in the dictionary than "+x+" is "+Merme+".")
+earth()
