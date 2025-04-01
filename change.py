c7e1d476c86736b4dbe84594c784ed7a45cd42f3
Joacobrule <jbrunengo@mail.austral.edu.ar>
2025-04-01 17:02:25 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,21 +1,21 @@
 def change():
-expense = 23.75
-money = 100
-P = Money-Expense
-P = int(P)
-B =Money-Expense
-C = B-P
-C =(C*100)
-C =int(C)
-print("Ingresar gasto:")
-print(Expense)
-print("Dinero recibido")
-print(Money)
-print("")
-print("Vuelto")
-print("")
-print("Pesos:")
-print(P)
-print("Centavos:")
-print(C)
+  expense = 23.75
+  money = 100
+  P = Money-Expense
+  P = int(P)
+  B =Money-Expense
+  C = B-P
+  C =(C*100)
+  C =int(C)
+  print("Ingresar gasto:")
+  print(Expense)
+  print("Dinero recibido")
+  print(Money)
+  print("")
+  print("Vuelto")
+  print("")
+  print("Pesos:")
+  print(P)
+  print("Centavos:")
+  print(C)
 change()
